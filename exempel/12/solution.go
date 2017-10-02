52b54cea63881e575c6b146258c525802217af1a
Gregory Vincic <g@7de.se>
2017-10-02 06:10:53 +0200

    Doc solution 12

--- exempel/12/solution.go
+++ exempel/12/solution.go
@@ -4,15 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func askNumber() int64 {
+	// Ask a question
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
+	fmt.Print("Enter number: ")
+	// Read the answer
 	line, _ := reader.ReadString('\n')
+	// Remove \n characters at end of line
 	noEnter := strings.Replace(line, "\n", "", 1)
+	// Convert the string to a number
 	number, _ := strconv.ParseInt(noEnter, 10, 64)
 	return number
 }
